91cb8cd9f417aa8b29b93c85547b9c722c3edd81
sekta2 <akovlevmihail535@gmail.com>
2026-07-05 18:06:39 +0400

    feat: add resource aluminum

--- lua/stekpacks/jmod/resources.lua
+++ lua/stekpacks/jmod/resources.lua
@@ -1,1 +1,12 @@
-print("to be implemented")
+stek.Resources.Add("jmod_aluminum", {
+    name = "Aluminum",
+    icon = "ez_resource_icons/aluminum.png",
+    volume = 1,
+    auto_entity = true,
+    entity = {
+        color = color_white,
+        material = "models/props_mining/ingot_jack_aluminum",
+        model = "models/jmod/resources/ingot001.mdl",
+        carryangles = Angle(0, 0, 0)
+    }
+})
