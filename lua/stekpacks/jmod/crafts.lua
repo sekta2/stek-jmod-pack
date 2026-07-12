cd9700faee64f2f1bb4234b585612bf886564ed0
sekta2 <akovlevmihail535@gmail.com>
2026-07-13 01:36:09 +0400

    feat: add toolbox craft

---
+++ lua/stekpacks/jmod/crafts.lua
@@ -0,0 +1,10 @@
+stek.Craft.Create("jmod_toolbox", {
+    name = "Toolbox",
+    description = "need for prikolz",
+
+    resources = {
+        jmod_aluminum = 50
+    },
+
+    output = function() end
+})
