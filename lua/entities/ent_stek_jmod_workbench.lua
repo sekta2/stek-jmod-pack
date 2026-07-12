35d4775892fe84cf8b93162a5e36c284a2ff5128
sekta2 <akovlevmihail535@gmail.com>
2026-07-13 01:35:37 +0400

    feat: add workbench entity

---
+++ lua/entities/ent_stek_jmod_workbench.lua
@@ -0,0 +1,15 @@
+AddCSLuaFile()
+
+---@diagnostic disable: assign-type-mismatch
+---@class ent_stek_jmod_workbench: ent_stek_craft_base
+ENT = ENT
+
+ENT.Base = "ent_stek_craft_base"
+ENT.Type = "anim"
+
+ENT.Spawnable = true
+ENT.PrintName = "Test workbench"
+ENT.Category = "STek: Machines"
+
+ENT.CraftType = "workbench"
+ENT.Model = "models/jmod/machines/workstations/weaponworkbench01.mdl"
