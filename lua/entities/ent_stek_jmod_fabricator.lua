8fe7f82c9d517574c3f292e37253b591a123ad37
sekta2 <akovlevmihail535@gmail.com>
2026-07-15 23:26:09 +0400

    feat: add primitive bench & fabricator

---
+++ lua/entities/ent_stek_jmod_fabricator.lua
@@ -0,0 +1,17 @@
+AddCSLuaFile()
+
+---@diagnostic disable: assign-type-mismatch
+---@class ent_stek_jmod_workbench: ent_stek_craft_base
+ENT = ENT
+
+ENT.Base = "ent_stek_craft_base"
+ENT.Type = "anim"
+
+ENT.Spawnable = true
+ENT.PrintName = "Fabricator"
+ENT.Category = "STek: Machines"
+
+ENT.CraftTable = "fabricator"
+ENT.Model = "models/jmod/machines/parts_machine.mdl"
+
+ENT.IconOverride = "entities/ent_jack_gmod_ezfabricator.png"
---
+++ lua/entities/ent_stek_jmod_primitivebench.lua
@@ -0,0 +1,17 @@
+AddCSLuaFile()
+
+---@diagnostic disable: assign-type-mismatch
+---@class ent_stek_jmod_workbench: ent_stek_craft_base
+ENT = ENT
+
+ENT.Base = "ent_stek_craft_base"
+ENT.Type = "anim"
+
+ENT.Spawnable = true
+ENT.PrintName = "Crafting Table"
+ENT.Category = "STek: Machines"
+
+ENT.CraftTable = "primitivebench"
+ENT.Model = "models/jmod/machines/workstations/primitive_bench.mdl"
+
+ENT.IconOverride = "entities/ent_jack_gmod_ezprimitivebench.png"
