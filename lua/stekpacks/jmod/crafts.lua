93acca52ed3d0a6968df56b4599d88a478c83abc
sekta2 <akovlevmihail535@gmail.com>
2026-07-15 07:55:56 +0400

    feat: add icon to toolbox craft

--- lua/stekpacks/jmod/crafts.lua
+++ lua/stekpacks/jmod/crafts.lua
@@ -1,6 +1,7 @@
 stek.Craft.Create("jmod_toolbox", {
     name = "Toolbox",
     description = "need for prikolz",
+    icon = "entities/ent_jack_gmod_eztoolbox.png",
 
     resources = {
         jmod_aluminum = 50
